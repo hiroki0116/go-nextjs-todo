b03b6eb6fcdece6c9c82376b3e3602a17c245dd4
hiroki0116 <hirokiseino0116@gmail.com>
2022-12-16 10:05:51 +1100

    Add link

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -1,7 +1,12 @@
+import Link from 'next/link';
+
 const Footer = () => {
   return (
     <footer className='relative text-center'>
-        <div className="fixed bottom-3 left-0 right-0 italic">© 2022, TaskGO.</div>
+        <div className="fixed bottom-3 left-0 right-0 flex flex-row gap-1 justify-center">
+          <Link href="https://60s-idea-training.vercel.app/about" className='tracking-tight text-primary font-bold bg-white/20 px-2 rounded'>About Developer</Link>
+          <div className='italic'>© 2022, TaskGO.</div>
+        </div>
     </footer>
   )
 }
