6ec13707c1ca2e994f5e2b290ba96e767ffde4fa
hiroki0116 <hirokiseino0116@gmail.com>
2022-12-15 15:10:55 +1100

    Change footer text

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className='relative text-center'>
-        <div className="fixed bottom-3 left-0 right-0">© 2022, Golang ToDo App.</div>
+        <div className="fixed bottom-3 left-0 right-0 italic">© 2022, TaskGO.</div>
     </footer>
   )
 }
