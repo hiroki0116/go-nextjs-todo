bcf8138bc93c08ea7e07a7d2647c34314247635a
hiroki0116 <hirokiseino0116@gmail.com>
2023-03-09 11:21:03 +1100

    Change css in body

--- components/Layout/LayoutWrapper.tsx
+++ components/Layout/LayoutWrapper.tsx
@@ -1,5 +1,5 @@
-const LayoutWrapper = ({children}:{children: any}) => {
-  return <div className="pt-36">{children}</div>
-}
+const LayoutWrapper = ({ children }: { children: any }) => {
+  return <div className="pt-36 mb-10">{children}</div>;
+};
 
-export default LayoutWrapper
+export default LayoutWrapper;
